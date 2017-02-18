7870d097be3383c9818515fc5ab2dd0360da4ceb
dalisoft <dalisoft@mail.ru>
2017-02-18 22:18:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * @name Tez.js
  * @description Lightweight, Flexible, Fast, Memory and Power Effecient Animation, Function and Class Manager
- * @version v2.1.3.0
+ * @version v2.1.4.0
  * @author @dalisoft (https://github.com/dalisoft)
  * @license Apache 2.0
  */
@@ -18,6 +18,32 @@
 	}
 }
 	(this, function (undefined) {
+		/*
+		Polyfills
+		 */
+		if (!Array.prototype.filter) {
+			Array.prototype.filter = function (fn) {
+				var i = 0;
+				while (i < this.length) {
+					if (!fn.call(this[i], this[i], i)) {
+						this.splice(i, 1);
+					} else {
+						i++
+					}
+				}
+				return this;
+			}
+		}
+		if (!Array.prototype.map) {
+			Array.prototype.map = function (fn, scope) {
+				var i = 0;
+				while (i < this.length) {
+					this[i] = fn.call(scope || this[i], this[i], i);
+					i++
+				}
+				return this;
+			}
+		}
 
 		var Tez = {};
 		/*
@@ -302,6 +328,101 @@
 			return new Tez.FunctionManager(fn)
 			.run(args);
 		};
+		Tez.DOMManager = function (node) {
+			this.node = node;
+			return this;
+		};
+		Tez.TweenManager = function (a, b) {
+			var _isFunc = typeof(b) === "function";
+			var _isArray = b && b.push && b.slice;
+			var _isObj = (!_isArray && typeof(b) === "object");
+			var _isNum = (!_isArray && !_isObj) && typeof(b) === "number";
+			var _isStr = !_isNum && typeof(b) === "string";
+			var _obj = {},
+			_arr = [],
+			_num = 0;
+			return function (t) {
+				if (_isFunc) {
+					return b(t);
+				} else if (_isArray) {
+					return (_arr = b.map(function (v2, i) {
+								if (typeof v2 === "number") {
+									return a[i] + (v2 - a[i]) * t;
+								} else if (typeof v2 === "function") {
+									return v2(t);
+								}
+							}));
+				} else if (_isObj) {
+					for (var p in b) {
+						if (typeof b[p] === "number") {
+							_obj[p] = a[p] + (b[p] - a[p]) * t;
+						} else if (typeof b[p] === "function") {
+							_obj[p] = b[p](t);
+						}
+					}
+					return _obj;
+				} else if (_isNum) {
+					return a + (b - a) * t;
+				}
+			}
+		};
+		Tez.DiffManager = function _rdh(a, b) {
+			if ((a && b) && (!a.nodeType && !b.nodeType) && typeof a === "object" && typeof b === "object") {
+				var _keys = (b && b.push && b.slice ? b : b && Object.keys(b)) || [],
+				_keysA = (a && a.push && a.slice ? a : a && Object.keys(a)) || [],
+				i = 1,
+				_diff = {},
+				_len = _keys && _keys.length,
+				_extract = ((a && _keys.length || 0) > (b && _keysA.length || 0) ? b : a) || _diff;
+				if (_keys.length || _keysA.length) {
+					for (var p in _extract) {
+						if (_rdh(a[p], b[p])) {
+							_diff[p] = [a[p], b[p]];
+							i++;
+						}
+					}
+				}
+				return Object.keys(_diff)
+				.length ? _diff : i > 1;
+			} else {
+				return a !== b && [a, b];
+			}
+		};
+		Tez.DOMManager.prototype = {
+			getNode: function () {
+				return this.node;
+			},
+			logic: function (fn) {
+				this._logic = new Tez.LogicManager(fn);
+				return this;
+			},
+			logicArg: function (arg) {
+				this._logic.run.call(this._logic, arg);
+				return this;
+			},
+			logicMsg: function (fn) {
+				this._logic && this._logic.onMessage(fn.call(this, this.node));
+				return this;
+			},
+			getLogic: function () {
+				return this._logic;
+			},
+			composite: function (fn) {
+				this._composite = new Tez.CompositeManager(fn.bind(this));
+				return this;
+			},
+			compositeArg: function (arg) {
+				this._composite.run.call(this._composite, arg);
+				return this;
+			},
+			compositeMsg: function (fn) {
+				this._composite && this._composite.onMessage(fn.call(this, this.node));
+				return this;
+			},
+			getComposite: function () {
+				return this._composite;
+			}
+		};
 		var attrs = function (a) {
 			if (!a)
 				return {};
@@ -584,6 +705,174 @@
 
 				this._vars.content = content;
 				return this._quickRender ? this.render() : this;
+			}
+		};
+		Tez.tezClass = function (opts) {
+			var _self = this;
+			this.events = {};
+			opts = Tez.extend(opts || {}, {
+					lets: {},
+					lets2: {},
+					_lets: {},
+					tweenable: {},
+					state: 0,
+					render: null
+				});
+
+			opts.setInitLets && opts.setInitLets.call(opts);
+			opts.setTweenableLets && opts.setTweenableLets.call(opts);
+
+			this.mountedNodes = [];
+
+			this.opts = opts;
+
+			this.lets = opts.lets;
+			this.lets2 = opts.lets2;
+			return this;
+		};
+		var getDecPow = function (d) {
+			return Math.pow(10, d || 4);
+		};
+		Tez.tezClass.prototype = {
+			plugin: function (plug) {
+				if (typeof plug === "string" && Tez.PluginManager[plug] !== undefined && Tez.PluginManager[plug].tez !== undefined) {
+					Tez.PluginManager[plug].tez.call(this, this.lets, this.opts);
+				}
+				return this;
+			},
+			apply: function () {
+				var _self = this,
+				opts = _self.opts,
+				lets = opts.lets,
+				lets2 = opts.lets2,
+				_lets = opts._lets;
+				var _minVal = 0.001,
+				_maxVal = 1
+					this.mountedNodes.map(function (node, index) {
+						var dom = new Tez.DOMManager(node);
+						var now = Date.now(),
+						render = opts.render,
+						tweenable = opts.tweenable,
+						round = tweenable.roundLets,
+						limitDec = tweenable.limitLetsDecimals,
+						start = tweenable && (typeof(tweenable.startTime) === "function" ? tweenable.startTime.call(_self, node, index) : tweenable.startTime) || 0,
+						dur = tweenable && (typeof(tweenable.duration) === "function" ? tweenable.duration.call(_self, node, index) : tweenable.duration) || 1000,
+						tween = Tez.TweenManager(lets, lets2),
+						diff;
+
+						if (tweenable) {
+							diff = Tez.DiffManager(lets, lets2);
+							dom.composite(function (e) {
+								if (!diff)
+									return e.state;
+								var elapsed = Math.max(0, Math.min(((Date.now() - now) - start) / dur, 1));
+								var timeElapsed = elapsed;
+								if (tweenable.curve) {
+									elapsed = tweenable.curve(timeElapsed);
+								}
+								_lets = tween(elapsed);
+								for (var p in _lets) {
+									if (round && round[p]) {
+										_lets[p] = typeof(_lets[p]) === "number" ? (_lets[p] | 0) : _lets[p] !== undefined && _lets[p].push && _lets[p].slice ? _lets[p].map(Math.round) : _lets[p];
+									} else if (limitDec && limitDec[p]) {
+										var dv = getDecPow(limitDec[p]);
+										_lets[p] = typeof(_lets[p]) === "number" ? (((_lets[p] * dv) | 0) / dv) : _lets[p] !== undefined && _lets[p].push && _lets[p].slice ? _lets[p].map(function (v) {
+												return ((v * dv) | 0) / dv;
+											}) : _lets[p];
+									}
+								}
+								if (timeElapsed > _minVal) {
+									render.call(_self, node, _lets, opts, index, elapsed);
+								}
+								return timeElapsed < _maxVal ? e.state : 0;
+							})
+							.compositeArg({
+								state: opts.state || "RUNNING"
+							});
+						} else if (opts.initState) {
+							render.call(_self, node, lets, opts, index, elapsed);
+						}
+
+					});
+				return this;
+			},
+			render: function (_lets, leaveLet) {
+				var _self = this,
+				opts = this.opts;
+				_lets = _lets || opts.lets;
+				var dm = Tez.DiffManager(opts.lets, _lets);
+				if (!dm)
+					return this;
+				this.mountedNodes.map(function (node, index) {
+					opts.render.call(_self, node, _lets || opts.lets, opts, index);
+				});
+				if (!leaveLet) {
+					opts.lets = _lets;
+				}
+				return this;
+			},
+			mountNode: function (node) {
+				node = ARRAY_SLICE.call(typeof(node) === "string" ? document.querySelectorAll(node) : node.length ? node : [node]);
+				this.mountedNodes = this.mountedNodes.concat(node);
+				return this;
+			},
+			nodeOn: function (event, on) {
+				var _self = this;
+				this.mountedNodes.map(function (node) {
+					_self.node = node;
+					node.addEventListener(event, function (e) {
+						on.call(_self, e);
+					});
+				});
+				return this;
+			},
+			nodeOff: function (event, off) {
+				var _self = this;
+				this.mountedNodes.map(function (node) {
+					_self.node = node;
+					node.removeEventListener(event, function (e) {
+						off.call(_self, e);
+					});
+				});
+				return this;
+			},
+			on: function (event, callback, unshift) {
+				if (!this.events[event]) {
+					this.events[event] = [];
+				}
+				this.events[event][unshift ? 'unshift' : 'push'](callback);
+				return this;
+			},
+			off: function (event, callback) {
+				if (!this.events[event].length) {
+					delete this.events[event];
+				}
+				var i = 0;
+				while (i < this.events[event].length) {
+					if (this.events[event][i] === callback) {
+						this.events[event].splice(i, 1);
+					} else {
+						i++
+					}
+				}
+				return this;
+			},
+			dispatch: function (event, custom) {
+				if (!this.events[event]) {
+					return;
+				}
+				custom = custom || {};
+				var _self = this,
+				opts = _self.opts;
+				this.events[event].map(function (event) {
+					event.call(_self, Tez.extend(custom, {
+							opts: opts,
+							timestamp: Date.now(),
+							type: event,
+							target: _self
+						}));
+				});
+				return this;
 			}
 		};
 		return Tez;
