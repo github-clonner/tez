7b0f20e42dea82f1f8aa0563245896a62769449d
dalisoft <dalisoft@mail.ru>
2017-05-15 00:10:15 +0500

    # Webpack update
    change(webpack): style fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,39 +1,43 @@
 const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
-const env  = require('yargs').argv.env; // use --env with webpack 2
+const env = require('yargs').argv.env; // use --env with webpack 2
 
-let libraryName = 'Tez';
+const libraryName = 'Tez';
 
 let plugins = [], outputFile;
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true, output: {comments: false} }));
-  outputFile = libraryName + '.min.js';
+	plugins.push(new UglifyJsPlugin({
+			minimize: true,
+			output: {
+				comments: false
+			}
+		}));
+	outputFile = libraryName + '.min.js';
 } else {
-  outputFile = libraryName + '.js';
+	outputFile = libraryName + '.js';
 }
 
 const config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname,
-    filename: outputFile,
-	libraryTarget: 'umd'
-  },
-  module: {
-	loaders: [
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  plugins: plugins
+	entry: __dirname + '/src/index.js',
+	devtool: 'source-map',
+	output: {
+		path: __dirname,
+		filename: outputFile,
+		libraryTarget: 'umd'
+	},
+	module: {
+		loaders: [{
+				test: /.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	},
+	plugins: plugins
 };
 
 module.exports = config;
